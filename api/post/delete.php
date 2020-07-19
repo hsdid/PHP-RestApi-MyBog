cd9bb23c8b2809c6524995b5ff27443eeb1e3db7
Mateusz Murawski <murawskimat00@gmail.com>
2020-07-19 14:19:31 +0200

    update and delete

--- api/post/delete.php
+++ api/post/delete.php
@@ -18,18 +18,18 @@ $post = new Post($db);
 // Get raw posted data 
 $data = json_decode(file_get_contents("php://input"));
 
-$post->title = $data->title;
-$post->body = $data->body;
-$post->author = $data->author;
-$post->category_id = $data->category_id;
+ // SET ID to update 
+$post->id = $data->id;
 
-//create post
+
+
+//DElete Post 
 if($post->create()) {
     echo json_encode(
-        array('message' => 'Post Created')
+        array('message' => 'Post Deleted')
     );
 } else {
     echo json_encode(
-        array('message' => 'Post Not Created')
+        array('message' => 'Post Not Deleted')
     );
 }
--- api/post/update.php
+++ api/post/update.php
@@ -18,18 +18,22 @@ $post = new Post($db);
 // Get raw posted data 
 $data = json_decode(file_get_contents("php://input"));
 
+ // SET ID to update 
+
+$post->id = $data->id;
+
 $post->title = $data->title;
 $post->body = $data->body;
 $post->author = $data->author;
 $post->category_id = $data->category_id;
 
-//create post
+//Update
 if($post->create()) {
     echo json_encode(
-        array('message' => 'Post Created')
+        array('message' => 'Post Update')
     );
 } else {
     echo json_encode(
-        array('message' => 'Post Not Created')
+        array('message' => 'Post Not Update')
     );
 }
